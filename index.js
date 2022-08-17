addc893acb99a8412625946f7ead6353c983a1ec
brightonchege <brightonspak@gmail.com>
2022-08-17 10:41:33 +0300

    arrows complete

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = () => {
+    return 2000/100
+    //console.log(2000/100)
+}
+
+
+const square = ( x )=> {
+    return x * x
+}
+square(2)
+
+const add = (a,b) => {
+    return a + b
+   // console.log(a+b)
+}
+add(3,4)
+
